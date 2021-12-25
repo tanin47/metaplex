be3d83219e4d31adc81baf7a32508260ff3e208c
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2021-12-25 11:44:40 +0700

    Remove explicit scopes

--- rust/token-metadata/program/src/processor.rs
+++ rust/token-metadata/program/src/processor.rs
@@ -34,9 +34,9 @@ use solana_program::{
 };
 use spl_token::state::{Account, Mint};
 
-pub fn process_instruction<'a>(
-    program_id: &'a Pubkey,
-    accounts: &'a [AccountInfo<'a>],
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     input: &[u8],
 ) -> ProgramResult {
     let instruction = MetadataInstruction::try_from_slice(input)?;
